f46721ed67fb0c2e1239ab4c52c7d2aabb889017
Gonzalo Ventura <gonzalogventura@gmail.com>
2022-10-17 11:05:02 -0300

    button y estructura

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
+import Winners from './components/Winners/Winners';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <h1>Sorteo</h1>
+      <Winners />
+      <Button />
     </div>
   );
 }
---
+++ src/components/Winners/Winners.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Winners = () => {
+  return (
+    <div className='list'>
+        <div className='winners'>
+            <span>GONZALO VENTURA</span>
+        </div>
+    </div>
+  )
+}
+
+export default Winners
