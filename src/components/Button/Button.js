e26654a0c6f283caf1f732cf321db4604c1c8023
Gonzalo Ventura <gonzalogventura@gmail.com>
2022-11-07 10:37:28 -0300

    sorteo beta

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import soundButton from "../assets/button.mp3";
 import sorteandoSound from "../assets/sorteando.mp3";
 import successSound from "../assets/success.mp3";
 
-const Button = ({sorteo, setSorteo}) => {
+const Button = ({data, sorteo, setSorteo}) => {
   const audio = new Audio(soundButton);
   const audioSorteando = new Audio(sorteandoSound);
   const audioSuccess = new Audio(successSound);
--- src/components/ListWinners/ListWinners.js
+++ src/components/ListWinners/ListWinners.js
@@ -3,8 +3,7 @@ import './ListWinners.scss'
 import Winners from '../Winners/Winners';
 import soundButton from "../assets/button.mp3";
 
-const ListWinners = ({sorteo, setSorteo, premiosSorteados, setPremiosSorteados}) => {
-    const data = ["Guillermo Montilla", "Maria Victoria Pagliaricci", "Malcom Azcurra Moriena", "Pablo Auliso", "Santiago Fiandino", "Maria Victoria Anomale", "Leandro Agusti", "Gonzalo Filipuzzi", "Mauricio Di Niro", "Nicolas Marin Moreno y Ana Laura Della Mea", "Gustavo A. Ardanaz", "Cristian Cazorla", "Guillermo José Ruival", "Pablo FERNANDEZ BARRON", "José Ariel Montilla", "Santiago Ardanaz", "Oscar Satorre y Cristian Cazorla", "Marcos Cesar Magnano", "Roberto Hume y Sabrina Pinedo", "Lucia Bolañes"]
+const ListWinners = ({data, sorteo, setSorteo, premiosSorteados, setPremiosSorteados}) => {
 
 
     const premios = [
@@ -66,7 +65,6 @@ const ListWinners = ({sorteo, setSorteo, premiosSorteados, setPremiosSorteados})
       audio.play();
       setSorteo("btn")
       setPremiosSorteados(premiosSorteados + 1)
-      console.log("sorteados: ", premiosSorteados)
     }
     
     return (
